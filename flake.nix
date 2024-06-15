fde1ba6b75aa315bca8689c9bbc4e4ac0f03c265
yemou <dev@lilac.pink>
2024-06-15 19:36:52 -0400

    Generate the config for each hostname
    stops me from needing to copy and paste the same snippet when I add
    a new server.

--- flake.nix
+++ flake.nix
@@ -1,65 +1,45 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    esquid = {
-      url = "github:Things-N-Stuff/eSquid";
-      # url = "path:/data/local-flakes/eSquid";
-    };
+    impermanence.url = "github:nix-community/impermanence";
+    sops-nix.url = "github:Mic92/sops-nix";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    impermanence.url = "github:nix-community/impermanence";
-    sops-nix.url = "github:Mic92/sops-nix";
+    esquid = {
+      url = "github:Things-N-Stuff/eSquid";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     yemou-dotfiles = {
       url = "git+https://codeberg.org/yemou/dotfiles";
       flake = false;
     };
     yemou-scripts = {
       url = "git+https://codeberg.org/yemou/scripts";
-      # url = "path:/data/local-flakes/scripts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, sops-nix, home-manager, impermanence, ... }@inputs: {
-    nixosConfigurations = {
-      lily = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = inputs;
-        modules = [
-          impermanence.nixosModules.impermanence
-          ./lily/config.nix
-          sops-nix.nixosModules.sops
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              extraSpecialArgs = inputs;
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.mou = import ./lily/home.nix;
-            };
-          }
-        ];
-      };
-      dandelion = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = inputs;
-        modules = [
-          impermanence.nixosModules.impermanence
-          ./dandelion/config.nix
-          sops-nix.nixosModules.sops
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              extraSpecialArgs = inputs;
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.mou = import ./dandelion/home.nix;
-            };
-          }
-        ];
-      };
+  outputs = { self, nixpkgs, sops-nix, home-manager, impermanence, ... }@inputs:
+    let genSystemConfigs = nixpkgs.lib.genAttrs [ "dandelion" "lily" ]; in {
+      nixosConfigurations = genSystemConfigs (hostname:
+        nixpkgs.lib.nixosSystem {
+          specialArgs = inputs;
+          modules = [
+            impermanence.nixosModules.impermanence
+            ./${hostname}/config.nix
+            sops-nix.nixosModules.sops
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                extraSpecialArgs = inputs;
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.mou = import ./${hostname}/home.nix;
+              };
+            }
+          ];
+        });
     };
-  };
 }
