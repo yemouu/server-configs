97da66afbc9682c13d471d1ff5d488fdaadd654a
yemou <dev@lilac.pink>
2024-06-28 14:36:29 -0400

    nixos-rebuild changes the TMPDIR variable using mktemp, so set TMPDIR before running nixos-rebuild

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,6 +1,21 @@
-{ ... }: {
+{ pkgs, ... }: {
   systemd = {
     services.nix-daemon.environment.TMPDIR = "/nix/tmp";
     tmpfiles.rules = [ "d /nix/tmp - root root 1d" ];
   };
+
+  # nixos-rebuild is a shellscript and inside that shellscript they create a tmpdir using mktemp.
+  # This is fine, but TMPDIR environment variable isn't available at this point, so mktemp puts the
+  # directory in the wrong place.
+  users.users.mou.packages = [
+    (pkgs.symlinkJoin {
+      name = "nixos-rebuild-tmpdir";
+      paths = [ pkgs.nixos-rebuild ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/nixos-rebuild \
+          --set TMPDIR /nix/tmp
+      '';
+    })
+  ];
 }
